49937e305d03c71f4ca02a3744ad49e4ec667744
Saketh reddy <yarabolusakethreddy2608@gmail.com>
2023-06-06 12:09:56 +0530

    sts testing

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Saketh Reddy
+ *
+ */
+module sts {
+}
---
+++ src/sts/sts.java
@@ -0,0 +1,12 @@
+package sts;
+
+public class sts {
+	int a=12;
+	
+	public static void main(String[] args) {
+		sts s=new sts();
+		s.a=12;
+		System.out.println(s.a);
+	}
+
+}
